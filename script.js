85fcd54393401329ce9a5b2ad0017539edb55cb7
User-42 <a.t.iafrate@gmail.com>
2020-12-10 22:58:55 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+//Loops--Lightning exercise 1 
+
+let i = 1
+
+while(i <= 100) {
+    console.log('TWEET TWEET')
+    i++
+}
+
+for(let i = 0; i < 100; i++){
+    console.log('TWEET TWEET TWEET')
+}
+
+//Loops lightning exercise 2
+// Favorite Foods
+// In your JavaScript, create a new variable called favoriteFoods. It should store an array of strings representing five of your favorite foods.
+// Loop through the array and print each food item to the console.
+
+let favoriteFoods = ['cheesecake', 'fajitas', 'grapes', 'anything curried']
+
+i = 0
+
+while(i < favoriteFoods.length){
+    console.log(favoriteFoods[i])
+    i++
+
+}
+
+for(let i = 0; i < favoriteFoods.length; i++) {
+    console.log(favoriteFoods[i])
+}
